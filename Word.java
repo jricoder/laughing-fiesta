4ce60d1356a35a4b78b8c6bea9be5dec1e156a6c
jricoder <jajack004@gmail.com>
2018-07-05 23:21:33 -0400

    First Commit

---
+++ Word.java
@@ -0,0 +1,35 @@
+package com.example.android.miwok;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+public class Word {
+
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mMiwokTranslation;
+
+    /** create the constructor which takes in two string parameters */
+    public Word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Get the default translaion of the word.
+     */
+    public String getmDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getmMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+}
